5c24e545cdf397fb8f89036c151203bdf3462d21
Md Jueal Hossain <juealdev@gmail.com>
2025-02-12 23:26:25 +0600

    fix:

--- src/app/(Pages)/page.js
+++ src/app/(Pages)/page.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div className=" bg-black text-white p-12 text-H1 font-bold">Home</div>
+        <div className=" bg-black text-white p-12 text-H1 font-bold">
+            Next js, React and Tailwind config app
+        </div>
     );
 };
 
